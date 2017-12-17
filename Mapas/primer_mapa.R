b643491868ac98e01263aa2d61791c9fd8286605
Centro tictank <tic.tank@urosario.edu.co>
2017-12-17 06:31:33 -0500

    El primer mejor mensaje del dia: Agregue los municipios :P

--- Mapas/primer_mapa.R
+++ Mapas/primer_mapa.R
@@ -12,23 +12,55 @@ library("reshape2")
 library("tidyverse")
 library("data.table")
 
+# Lectura del shape de Colombia por departamento
 
-# Lectura del shape de Colombia
-
-shape <- readShapeSpatial("Mapas/COL_adm2.shp", repair = T)
-shape1 <- readShapeSpatial("Mapas/Nuevos_mapas/depto.shp", repair = T)
-mapa_ordenado <- fortify(shape1, region = "DPTO") %>%  data.table()
+shape_dpto <- readShapeSpatial("Mapas/Nuevos_mapas/depto.shp", repair = T)
+mapa_ordenado <- fortify(shape, region = "DPTO") %>%  data.table()
 mapa_ordenado[, id := as.integer(id)]
-mapa_anterior <- fortify(shape, region = "NAME_2") %>%  data.table()
+
+# Lectura del shape de Colombia por municipio
+
+shape_mpio <- readShapeSpatial("Mapas/Nuevos_mapas/mpio.shp", repair = T)
+mapa_ordenado_mpio <- fortify(shape_mpio, region = "MPIO") %>%  data.table()
+mapa_ordenado_mpio[, id := as.integer(id)]
 
 # lectura de divipola
 
 divipola <- read.delim("divipola.csv", header = T, sep = ",", encoding = "UTF-8") %>% data.table()
-datos_simulados <- divipola[, .(conteo = length(MUNICIPIO)), by = c("DEPARTAMENTO", "CÓDIGO.DANE.DEL.DEPARTAMENTO")] %>% setnames(names(.), c("dpto", "cod_dpto", "conteo"))
+datos_simulados <- divipola[, .(conteo = length(MUNICIPIO)), by = c("DEPARTAMENTO", "C�DIGO.DANE.DEL.DEPARTAMENTO")] %>% setnames(names(.), c("dpto", "cod_dpto", "conteo"))
+
 
 # Creación del mapa
 
-p <- ggplot() + geom_map(data = mapa_ordenado, aes(map_id = id), map = mapa_ordenado, color = "aliceblue", fill = "red")+
+p <- ggplot() + geom_map(data = mapa_ordenado, aes(map_id = id), map = mapa_ordenado, color = "aliceblue", fill = "gray")+
   geom_map(data = datos_simulados, aes(map_id = cod_dpto, fill = conteo), 
            map = mapa_ordenado) + expand_limits(x = mapa_ordenado$long, y = mapa_ordenado$lat)
 p
+
+
+# lectura de divipola
+
+divipola <- read.delim("divipola.csv", header = T, sep = ",", encoding = "UTF-8") %>% data.table() %>% setnames(names(.), c("region", "cod_dpto", "dpto", "cod_mpio", "mpio"))
+divipola[, caracteres := nchar(cod_dpto)]
+divipola[, cod_mpio1 := substr(cod_mpio, caracteres +1, nchar(cod_mpio))]
+datos_simulados <- divipola[, .(conteo = length(mpio)), by = c("mpio", "cod_mpio1", "cod_dpto")]
+datos_simulados[, conteo := as.integer(round(rnorm(nrow(datos_simulados), 100, 20)))]
+
+p1 <- ggplot() + geom_map(data = mapa_ordenado_mpio, aes(map_id = id), map = mapa_ordenado_mpio, color = "aliceblue", fill = "gray")+
+  geom_map(data = datos_simulados, aes(map_id = cod_mpio1, fill = conteo), 
+           map = mapa_ordenado_mpio) + expand_limits(x = mapa_ordenado_mpio$long, y = mapa_ordenado_mpio$lat)
+p1
+
+# Tomando un subconjunto de los datos
+
+datos_simulados_mpio <- datos_simulados[cod_dpto == 54]
+mapa_ordenado_mpio1 <- mapa_ordenado_mpio[ id %in% datos_simulados_mpio$cod_mpio1]
+
+mapa_ordenado_mpio1[, hole := "FALSE"]
+mapa_ordenado_mpio1[, piece := 1]
+
+
+p1 <- ggplot() + geom_map(data = mapa_ordenado_mpio1, aes(map_id = id), map = mapa_ordenado_mpio1, color = "aliceblue", fill = "gray")+
+  geom_map(data = datos_simulados_mpio, aes(map_id = cod_mpio1, fill = conteo), 
+           map = mapa_ordenado_mpio1) + expand_limits(x = mapa_ordenado_mpio1$long, y = mapa_ordenado_mpio1$lat)
+p1
