46f0a830e052d339bfe5893de69c705f3866b7a4
Centro tictank <tic.tank@urosario.edu.co>
2017-12-17 19:20:10 -0500

    Mapas y diferentes colores

--- Mapas/mapas_cuantiles.R
+++ Mapas/mapas_cuantiles.R
@@ -1,6 +1,7 @@
 library(dplyr)
 library(ggplot2)
 library(data.table)
+library(RColorBrewer)
 
 shape_municipios <- read.delim("shape_municipios.txt") %>% data.table()
 # Uso 
@@ -13,16 +14,59 @@ datos_simulados22[, clasificacion := ifelse(conteo < quantile(conteo, 2/3), "Niv
 datos_simulados22[, clasificacion := ifelse(conteo < quantile(conteo, 1/3), "Nivel 3", clasificacion)]
 datos_simulados22[, clasificacion := factor(clasificacion)]
 
+datos_simulados22[, clasificacion5 := "Nivel 1"]
+datos_simulados22[, clasificacion5 := ifelse(conteo < quantile(conteo, 4/5), "Nivel 2", clasificacion5)]
+datos_simulados22[, clasificacion5 := ifelse(conteo < quantile(conteo, 3/5), "Nivel 3", clasificacion5)]
+datos_simulados22[, clasificacion5 := ifelse(conteo < quantile(conteo, 2/5), "Nivel 4", clasificacion5)]
+datos_simulados22[, clasificacion5 := ifelse(conteo < quantile(conteo, 1/5), "Nivel 5", clasificacion5)]
+datos_simulados22[, clasificacion5 := factor(clasificacion5)]
 
 
-ggplot() +
+map <- ggplot() +
   geom_map(data = shape_predeterminado, aes( map_id = id), map = shape_predeterminado, color = "aliceblue", fill = "gray") +
   geom_map(data = datos_simulados22, aes(map_id = id, fill = clasificacion), map = shape_predeterminado) +
   geom_map(data = shape_predeterminado, aes( map_id = id), map = shape_predeterminado, color = "white", fill = NA) +
   expand_limits(x = shape_predeterminado$long, y = shape_predeterminado$lat) +
-  scale_fill_manual(values = c("#00C1FF", "#D0E100", "#FF2E00"),
+  scale_fill_manual(values = c("#36E00B", "#F1E818", "#F10034"),
                     labels = c("Bueno", "Regular", "Malo"),
                     name = "Clasificación")
+map
+
+map <- ggplot() +
+  geom_map(data = shape_predeterminado, aes( map_id = id), map = shape_predeterminado, color = "aliceblue", fill = "gray") +
+  geom_map(data = datos_simulados22, aes(map_id = id, fill = clasificacion5), map = shape_predeterminado) +
+  geom_map(data = shape_predeterminado, aes( map_id = id), map = shape_predeterminado, color = "white", fill = NA) +
+  expand_limits(x = shape_predeterminado$long, y = shape_predeterminado$lat)+
+  scale_fill_manual(values = colorRampPalette(brewer.pal(5,"Greens"))(5),
+                    name = "Clasificación")
+
+map
+
+map <- ggplot() +
+  geom_map(data = shape_predeterminado, aes( map_id = id), map = shape_predeterminado, color = "aliceblue", fill = "gray") +
+  geom_map(data = datos_simulados22, aes(map_id = id, fill = clasificacion5), map = shape_predeterminado) +
+  geom_map(data = shape_predeterminado, aes( map_id = id), map = shape_predeterminado, color = "white", fill = NA) +
+  expand_limits(x = shape_predeterminado$long, y = shape_predeterminado$lat)+
+  scale_fill_manual(values = colorRampPalette(brewer.pal(5,"Blues"))(5),
+                    name = "Clasificación")
+
+map
 
 
+map <- ggplot() +
+  geom_map(data = shape_predeterminado, aes( map_id = id), map = shape_predeterminado, color = "aliceblue", fill = "gray") +
+  geom_map(data = datos_simulados22, aes(map_id = id, fill = conteo), map = shape_predeterminado) +
+  geom_map(data = shape_predeterminado, aes( map_id = id), map = shape_predeterminado, color = "white", fill = NA) +
+  expand_limits(x = shape_predeterminado$long, y = shape_predeterminado$lat) +
+  scale_fill_gradient(low = "#FF7400",
+                      high ="#36E00B",
+                      name = "Clasificación")
+map
 
+map <- ggplot() +
+  geom_map(data = shape_predeterminado, aes( map_id = id), map = shape_predeterminado, color = "aliceblue", fill = "gray") +
+  geom_map(data = datos_simulados22, aes(map_id = id, fill = conteo), map = shape_predeterminado) +
+  geom_map(data = shape_predeterminado, aes( map_id = id), map = shape_predeterminado, color = "white", fill = NA) +
+  expand_limits(x = shape_predeterminado$long, y = shape_predeterminado$lat) +
+  scale_fill_gradient(name = "Clasificación")
+map
